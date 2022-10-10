46e6a97936037ce6052e255cc96803066fd879e3
Saikat Saha <64367664+Saikatsaha798@users.noreply.github.com>
2022-10-10 18:01:35 +0530

    Variable number of numbers can be added

--- super.c
+++ super.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
 {
-  int a,b,sum; 
-  printf("Enter a and  b : ");
-  scanf("%d %d", &a, &b);
-  sum = a + b;
+  int i, a, num, sum = 0; 
+  printf("Enter number of Element : ");
+  scanf("%d", &num);
+  printf("Enter the Numbers : ");
+  for (i=0; i<num; i++){
+      scanf("%d", &a);
+      sum += a;
+  }
+ 
   printf("Sum : %d", sum);
 }
